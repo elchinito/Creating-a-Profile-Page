ed432e4aa71faa3f4fb57fe63f5111e19e1131e9
elchinito <31776005+elchinito@users.noreply.github.com>
2022-11-16 20:20:28 -0800

    Added index.js code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let content = document.querySelector('.js-generated.content')
 
 let header = document.createElement('h1')
 header.setAttribute('class', 'dog-name')
-header.append('Rizzo')
+header.append('Lizzo')
 content.append(header)
 
 let dogContent = document.createElement('div')
@@ -18,5 +18,30 @@ content.append(dogImage)
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
 content.append(dogDetails)
+
+let detailHeader = document.createElement('h3');
+detailHeader.append('Description:');
+dogDetails.append(detailHeader);
+
+let descriptionPara = document.createElement('p');
+descriptionPara.append('This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals.');
+dogDetails.append(descriptionPara);
+
+let feedingHeader = document.createElement('h3');
+feedingHeader.append('Feeding Times:');
+dogDetails.append(feedingHeader);
+
+let feedingTimes = document.createElement('ul');
+let timeOne = document.createElement('li');
+timeOne.append('9:00 am');
+let timeTwo = document.createElement('li');
+timeTwo.append('12:00 pm');
+let timeThree = document.createElement('li');
+timeThree.append('5:00 pm');
+feedingTimes.append(timeOne);
+feedingTimes.append(timeTwo);
+feedingTimes.append(timeThree);
+
+dogDetails.append(feedingTimes);
 
 
